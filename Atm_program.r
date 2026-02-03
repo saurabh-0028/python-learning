778a3ef63bcb33400216303e00235f6f3a1183c5
mr selfish <saurabh092415@gmail.com>
2026-02-03 23:27:39 +0530

    python_atm_simulation

---
+++ Atm_program.r
@@ -0,0 +1,16 @@
+for i in range(3):
+  p=int(input("Enter pin number:"))
+  if (p==pin):
+    print("correct pin")
+    wb=int(input("Enter amount:"))
+    if(cb>wb):
+      cb=cb-wb
+      print("Transection successful.")
+    else:
+      print("Insufficient balance")
+
+    break
+  else:
+    print("Incorrect pin number")
+else:
+    print("card block")
