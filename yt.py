bcc29471891505dfb11731d9f118a0b860da7941
mr selfish <saurabh092415@gmail.com>
2026-07-29 22:04:09 +0530

    Add files via upload

---
+++ yt.py
@@ -0,0 +1,14 @@
+x=input("Enter your gf name:")
+if x == "jyoti":
+    print("haa sahi hai")
+else:
+    print("wrong Name")
+    print("sayad tum apni gf ka name phul gaye")
+    print("accha chalo uska age batao")
+y=input("Enter your gf age:")
+if y== "21":
+    print("haa",y,"tumhe to ab bhi yad hai")
+else:
+    print("nahii,Thik se yadd karoo")
+input("Press any button to continue")
+
